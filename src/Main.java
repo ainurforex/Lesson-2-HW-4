44d06a37dde015e51236ebdf44ccd2ac2bc076f6
Ainur <ainurforex@mail.ru>
2022-06-06 23:48:21 +0500

    Урок 2 ДЗ 4 fix

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         int targetWeightReductionKg=7;
+        int grInKg=1000;
         int weightReductionRation1Gr=250;
         int weightReductionRation2Gr=500;
         int targetWeightReductionGr;
         int dayReductionRation1;
         int dayReductionRation2;
-        int avarageReduction;
-        targetWeightReductionGr=targetWeightReductionKg*1000;
+        float avarageReduction;
+        targetWeightReductionGr=targetWeightReductionKg*grInKg;
         dayReductionRation1=targetWeightReductionGr/weightReductionRation1Gr;
         System.out.println("Для достижения снижения веса на 7 кг, у спортсмена,");
         System.out.println("при снижении на 250 гр в день, уйдет "+dayReductionRation1+" дней,");
         dayReductionRation2=targetWeightReductionGr/weightReductionRation2Gr;
         System.out.println("при снижении на 500 гр в день, уйдет "+dayReductionRation2+" дней.");
-        avarageReduction=(dayReductionRation1+dayReductionRation2)/2;
+        avarageReduction=(dayReductionRation1+dayReductionRation2)/((float)2);
         System.out.println("Средний показатель достижения снижения веса "+avarageReduction+" день.");
     }
 }
